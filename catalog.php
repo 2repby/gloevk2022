7e6853cb1174362ee07d726bcd393b0c6dde911c
kda <2repby@gmail.com>
2022-11-28 19:17:11 +0500

    Item page, Application entry point

--- catalog.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('dbconnect.php');
-$result = $conn->query("SELECT * FROM products");
-require('components/header.php');
-require('components/products_list.php');
-require('components/footer.php');
-?>
-
